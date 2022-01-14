8e8d4f3012d0b59c467d699bc9df53be217a92ca
dhoine345 <lue42@yandex.ru>
2022-01-14 10:34:23 +0300

    script

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup')
-let closeButton = document.querySelector('.popup__close-button')
-let submitButton = document.querySelector('.form__submit-button')
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let submitButton = document.querySelector('.form__submit-button');
+let popupForm = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.form__input-name');
+let jobInput = document.querySelector('.form__input-job');
+let profileAvatarName = document.querySelector('.profile__avatar-name');
+let profileAvatarHobby = document.querySelector('.profile__avatar-hobby');
 
 editButton.addEventListener('click', function() {
   popup.style = `display: grid`;
@@ -12,5 +17,16 @@ closeButton.addEventListener('click', function() {
 });
 
 submitButton.addEventListener('click', function() {
-  
+  popup.style = `display: none`;
 });
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileAvatarName.textContent = nameInput.value;
+  profileAvatarHobby.textContent = jobInput.value;
+};
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
+
+
