6aa92d32bdc4e264c6d3fedbc1fa4941f52e6a00
dhoine345 <lue42@yandex.ru>
2022-01-15 12:54:36 +0300

    like

--- script.js
+++ script.js
@@ -7,18 +7,25 @@ let nameInput = document.querySelector('.popup__container-form-input-name');
 let jobInput = document.querySelector('.popup__container-form-input-job');
 let profileAvatarName = document.querySelector('.profile__avatar-name');
 let profileAvatarHobby = document.querySelector('.profile__avatar-job');
+let elementLike = document.querySelectorAll('.element__like');
 
 editButton.addEventListener('click', function() {
-  popup.style = `display: block`;
+  popup.style = 'display: block';
 });
 
 closeButton.addEventListener('click', function() {
-  popup.style = `display: none`;
+  popup.style = 'display: none';
 });
 
 submitButton.addEventListener('click', function() {
-  popup.style = `display: none`;
+  popup.style = 'display: none';
 });
+
+for (let i = 0; i < elementLike.length; i += 1) {
+ elementLike[i].addEventListener('click', function() {
+ elementLike[i].style = 'background-image: url(./image/like_active.svg;)';
+  });
+};
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -27,6 +34,4 @@ function formSubmitHandler (evt) {
 };
 
 popupForm.addEventListener('submit', formSubmitHandler);
-
-
 
