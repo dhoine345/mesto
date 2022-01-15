5cf1455af79e6da5268ebf70ee9526684d0d8b97
dhoine345 <lue42@yandex.ru>
2022-01-15 08:39:36 +0300

    изменил названия элементов

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button');
-let submitButton = document.querySelector('.form__submit-button');
-let popupForm = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.form__input-name');
-let jobInput = document.querySelector('.form__input-job');
+let closeButton = document.querySelector('.popup__container-form-close-button');
+let submitButton = document.querySelector('.popup__container-form-submit-button');
+let popupForm = document.querySelector('.popup__container-form');
+let nameInput = document.querySelector('.popup__container-form-input-name');
+let jobInput = document.querySelector('.popup__container-form-input-job');
 let profileAvatarName = document.querySelector('.profile__avatar-name');
 let profileAvatarHobby = document.querySelector('.profile__avatar-job');
 
 editButton.addEventListener('click', function() {
-  popup.style = `display: grid`;
+  popup.style = `display: block`;
 });
 
 closeButton.addEventListener('click', function() {
