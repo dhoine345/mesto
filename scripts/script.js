1ffd432926ba24cc55f3471b833a3badb2b4c922
dhoine345 <lue42@yandex.ru>
2022-01-18 11:54:49 +0300

    Исправления после первой итерации

--- scripts/script.js
+++ scripts/script.js
@@ -8,32 +8,26 @@ let jobInput = document.querySelector('.popup__container-form-input_type_job');
 let profileAvatarName = document.querySelector('.profile__avatar-name');
 let profileAvatarHobby = document.querySelector('.profile__avatar-job');
 
-
+//Функции
 function openPopup () {
-  
-}
-
-editButton.addEventListener('click', function() {
   popup.classList.toggle('popup_opened');
-});
+};
 
-closeButton.addEventListener('click', function() {
-  popup.style = 'display: none';
-});
-
-
-function confirmPopup () {
-  popup.style = 'display: none';
+function removePopup () {
+  popup.classList.remove ('popup_opened');
 };
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileAvatarName.textContent = nameInput.value;
   profileAvatarHobby.textContent = jobInput.value;
-  confirmPopup ();
+  removePopup ();
 };
 
-popupForm.addEventListener('submit', formSubmitHandler);
+//Слушатели
+editButton.addEventListener('click', openPopup); //открыть форму редактирования профайла
+closeButton.addEventListener('click', removePopup); //закрыть форму редактирования профайла
+popupForm.addEventListener('submit', formSubmitHandler); //подтвердить ввод новых данных в профайле
 
 /*for (let i = 0; i < elementLike.length; i += 1) {
  elementLike[i].addEventListener('click', function() {
