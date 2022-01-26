88d3d32b3a7efa0190de50020d3e2bdffb6cdba4
dhoine345 <lue42@yandex.ru>
2022-01-26 16:41:49 +0300

    fixed

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+//Переменные
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__container-form-close-button');
+let popupForm = document.querySelector('.popup__container-form');
+let nameInput = document.querySelector('.popup__container-form-input_type_name');
+let jobInput = document.querySelector('.popup__container-form-input_type_job');
+let profileAvatarName = document.querySelector('.profile__avatar-name');
+let profileAvatarHobby = document.querySelector('.profile__avatar-job');
+
+//Функции
+function openPopup () {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileAvatarName.textContent; //неправильно понял замечания еще при первой итерации ¯\_(ツ)_/¯
+  jobInput.value = profileAvatarHobby.textContent;
+};
+
+function removePopup () {
+  popup.classList.remove('popup_opened');
+};
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileAvatarName.textContent = nameInput.value;
+  profileAvatarHobby.textContent = jobInput.value;
+  removePopup ();
+};
+
+//Слушатели
+editButton.addEventListener('click', openPopup); //открыть форму редактирования профайла
+closeButton.addEventListener('click', removePopup); //закрыть форму редактирования профайла
+popupForm.addEventListener('submit', formSubmitHandler); //подтвердить ввод новых данных в профайле
+
+/*for (let i = 0; i < elementLike.length; i += 1) {
+ elementLike[i].addEventListener('click', function() {
+ elementLike[i].classList.add('element__like_active');
+ });
+};*/
+/*let elementLike = document.querySelectorAll('.element__like');*/
