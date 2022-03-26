20f3c60412ac193f178b75981128c7d9864e4448
dhoine345 <lue42@yandex.ru>
2022-03-26 13:43:28 +0300

    ПР8

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+        'postcss-loader']
       }
     ]
   },
@@ -44,5 +46,5 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
-  ]
+  ],
 }
